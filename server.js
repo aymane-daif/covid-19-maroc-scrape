d615655eeb817a7427538ab9126fe4916f88ccf2
Aymane Daif <aymaned345@gmail.com>
2022-02-27 14:51:36 +0100

    :tada: get static files from dist

--- server.js
+++ server.js
@@ -22,9 +22,9 @@ app.get('/api/covid/results', async (req, res) => {
 
 // serve static assaets if where are in production
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+  app.use(express.static('client/dist'));
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'));
   });
 }
 const PORT = process.env.PORT || 1947;

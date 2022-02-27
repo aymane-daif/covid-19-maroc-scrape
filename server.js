4cfbc0f44c12f122f968f702cd5f96cf65606274
Aymane Daif <aymaned345@gmail.com>
2022-02-27 14:31:40 +0100

    :tada: update path to client folder

--- server.js
+++ server.js
@@ -22,11 +22,9 @@ app.get('/api/covid/results', async (req, res) => {
 
 // serve static assaets if where are in production
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.use(express.static('client/build'));
   app.get('*', (req, res) => {
-    res.sendFile(
-      path.resolve(__dirname, '..', 'client', 'build', 'index.html')
-    );
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 const PORT = process.env.PORT || 1947;

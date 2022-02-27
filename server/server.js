7ca27885a20f990bbcb09d6526513efddf776ba2
Aymane Daif <aymaned345@gmail.com>
2022-02-27 13:40:12 +0100

    :sparkles: deploy to heroku

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 
 const { scrapeData } = require('./scrape');
 
@@ -19,5 +20,12 @@ app.get('/api/covid/results', async (req, res) => {
   res.json(scrapedData);
 });
 
+// serve static assaets if where are in production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 const PORT = process.env.PORT || 1947;
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
